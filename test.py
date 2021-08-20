cb534283e08af2a2197c4ec8870d7591bf2edc6f
shabu <67824592+khus-k@users.noreply.github.com>
2021-08-20 16:26:25 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,70 @@
+total_classes=['Englis','Maths','Science','French','Computer','Music','Yoga']
+classes={
+    'English':{
+        '1to3':{
+            'days':['monday','wednesday'],
+            'timeing':'10:30 to 11:30',
+            'students_name':['Lali','Veer','Abhinash','Kajal','Akash','Gunjan','Sudash','Shivam','Rajkumar','Asif','Sachin'],
+            'teacher_name':'prerna_mam',
+        },
+        '4to5':{
+            'days':['monday','wednesday'],
+            'timeing':'11:30 to 12:30',
+            'students_name':['simran','umresh','Tushar','Neha','Komal','Davraj','Naresh'],
+            'teacher_name':'Prerna_Mam',
+        },
+        '6to8':{
+            'days':['tuesday','thursday'],
+            'timeing':'10:30 to 11:30',
+            'students_name':['Nargis','Geeta','Sohan','vishal small','vishal big','raju'],
+            'teacher_name':'prerna_mam',
+        },
+        '9to12':{
+            'days':['tuesday','thursday'],
+            'timeing':'11:30 to 12:30',
+            'students_name':['Khushi','Rani','Siddhart','Pooja','Shama','abhishek'],
+            'teacher_name':'prerna_mam',
+        }
+    },
+    'Maths_Science_core_team':{
+        'day':'saturday',
+        'timeing':'2:00 to 4:00',
+        'students_name':['Nargis','Khushi','Rani','Abhuishek','Shama','siddhart'],
+        'teachers_name':['Babli_mam','Ritika_mam','Ashwani_sir']
+    },
+    'Math_younger_student':{
+        'day':'wednesday',
+        'timeing':'2:30 to 4:00',
+        'students_name':['Tushar','sohan','Raju','Rihana','Alak','Devraj','vishal small','vishal big','Geeta','Umresh','Pooja','Simran','komal'],
+        'teacher_name':'Tarun_sir' 
+    },
+    'French':{
+        'days':['tuesday','thursday'],
+        'timeing':'2:00 to 3:00',
+        'students_name':['Nargis','Shama','Siddhart','Rani','geeta'],
+        'teacher_name':'Mahima_Mam'
+    },
+    'computer':{
+        'day':'saturday',
+        'timeing':'2:00 to 4:00',
+        'students_name':['Khushi','Nargis','siddhart'],
+        'teacher_name':'Hanish_Sir'
+    },
+    'Musci':{
+        'day':'friday',
+        'timeing':'11:00 to 12:00',
+        'students_name':['Khushi','Simran','Rani','Vishal small','geeta','shama','sachin','Alka','Umresh','siddhart','abhishek','Devraj','Pooja','Rihana','Raju','tushar','subash','Lali','sohan'],
+        'teacher_name':['vishal Sir','Prite Mam']
+    },
+    'yoga':{
+        'day':['Monday','tuesday','wednesday','thursday','friday','saturday'],
+        'timeing':'6:00 to 8:00 am',
+        'students_name':["aakash","aasif","manish","veer","annu","rajkumar","shiyam","komal","lali","kitt",
+        "simran","alka","rani","geeta","nargis","pooja","shama","jyoti","kushi","aarti","umeresh","sid","abhi","sachin",
+        "vishal","vishal b","sohan","raju","dev","tushar","aashish","rahul","mahua","mannu","vijay","raju"],
+        'teacher_name':['Rahul sir','Nargis','Siddharth']
+
+    }    
+}
+
+print (classes)
